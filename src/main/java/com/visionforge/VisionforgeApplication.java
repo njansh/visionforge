580c0ec78fcbdd8b7d38b6ec63d68bf769c20e29
Njansh <nadsonjhonyobmep@gmail.com>
2026-02-24 20:31:05 -0300

    chore: initialize Spring Boot project with base dependencies

---
+++ src/main/java/com/visionforge/VisionforgeApplication.java
@@ -0,0 +1,13 @@
+package com.visionforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionforgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/visionforge/VisionforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visionforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
